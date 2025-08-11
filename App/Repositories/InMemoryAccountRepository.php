8280a28556c0e36b70425549b7a177c65b96a4d1
Caio Padilha <caiocfpadilha@gmail.com>
2025-08-11 16:04:57 -0300

    refactor: remove validation in repositories to centralize in service classes

--- App/Repositories/InMemoryAccountRepository.php
+++ App/Repositories/InMemoryAccountRepository.php
@@ -4,9 +4,7 @@ namespace App\Repositories;
 
 use App\Exceptions\AccountAlreadyExistsException;
 use App\Exceptions\AccountNotFoundException;
-use App\Exceptions\InvalidAmountException;
 use App\Repositories\Interfaces\AccountRepositoryInterface;
-use InvalidArgumentException;
 
 class InMemoryAccountRepository implements AccountRepositoryInterface {
     /**
@@ -15,8 +13,6 @@ class InMemoryAccountRepository implements AccountRepositoryInterface {
     private array $accounts = [];
 
     public function createAccount(int $accountId): void {
-        $this->isValidAccountIdOrFail($accountId);
-
         if (isset($this->accounts[$accountId])) {
             throw new AccountAlreadyExistsException("Account with ID {$accountId} already exists.");
         }
@@ -26,9 +22,6 @@ class InMemoryAccountRepository implements AccountRepositoryInterface {
 
     public function deposit(int $accountId, float $amount): float
     {
-        $this->isValidAccountIdOrFail($accountId);
-        $this->isAmountOrFail($amount);
-
         if (!isset($this->accounts[$accountId])) {
             throw new AccountNotFoundException("Account with ID {$accountId} does not exist.");
         }
@@ -36,31 +29,5 @@ class InMemoryAccountRepository implements AccountRepositoryInterface {
         $this->accounts[$accountId] += $amount;
 
         return $this->accounts[$accountId];
-    }
-
-    /**
-     * Validates the account ID.
-     *
-     * @throws InvalidArgumentException if the ID is not a positive integer.
-     * @param integer $accountId
-     * @return void
-     */
-    private function isValidAccountIdOrFail(int $accountId): void {
-        if ($accountId <= 0) {
-            throw new InvalidArgumentException("Account ID must be a positive integer.");
-        }
-    }
-
-    /**
-     * Validates the amount.
-     *
-     * @throws InvalidAmountException if the amount is not a positive number.
-     * @param float $amount
-     * @return void
-     */
-    private function isAmountOrFail(float $amount): void {
-        if ($amount <= 0) {
-            throw new InvalidAmountException("Amount must be a positive number.");
-        }
     }
 }
--- tests/Repositories/InMemoryAccountRepositoryTest.php
+++ tests/Repositories/InMemoryAccountRepositoryTest.php
@@ -4,7 +4,6 @@ use App\Exceptions\AccountAlreadyExistsException;
 use PHPUnit\Framework\TestCase;
 
 use App\Exceptions\AccountNotFoundException;
-use App\Exceptions\InvalidAmountException;
 use App\Repositories\InMemoryAccountRepository;
 
 class InMemoryAccountRepositoryTest extends TestCase
@@ -25,17 +24,11 @@ class InMemoryAccountRepositoryTest extends TestCase
         $this->assertEquals(1.0, $balance);
     }
 
-    public function testCreateAccountInitializesWithInvalidAccountId()
-    {
-        $accountId = -1;
-        $this->expectException(InvalidArgumentException::class);
-        $this->repository->createAccount($accountId);
-    }
-
-    public function testDuplicateCreateAccount()
+    public function testDuplicateCreateAccountThrowsException()
     {
         $accountId = 1;
         $this->repository->createAccount($accountId);
+
         $this->expectException(AccountAlreadyExistsException::class);
         $this->repository->createAccount($accountId);
     }
@@ -50,27 +43,9 @@ class InMemoryAccountRepositoryTest extends TestCase
         $this->assertEquals(75.0, $balance);
     }
 
-    public function testDepositIncreasesBalanceWithInvalidAmount()
-    {
-        $accountId = 1;
-        $this->repository->createAccount($accountId);
-
-        $this->expectException(InvalidArgumentException::class);
-        $this->repository->deposit($accountId, -1);
-    }
-
-    public function testDepositThrowsExceptionForInvalidAmount()
-    {
-        $this->expectException(InvalidAmountException::class);
-        $accountId = 3;
-        $this->repository->createAccount($accountId);
-        $this->repository->deposit($accountId, 0);
-    }
-
     public function testDepositThrowsExceptionForNonExistentAccount()
     {
         $this->expectException(AccountNotFoundException::class);
         $this->repository->deposit(999, 10);
     }
-
 }
