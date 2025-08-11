5cecb1f56cc45f607f011d24222e8c88f2244806
Caio Padilha <caiocfpadilha@gmail.com>
2025-08-11 15:41:38 -0300

    feat: add AccountService interface to define service layer contract for account operations

---
+++ App/Services/Interfaces/AccountServiceInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services\Interfaces;
+
+use App\Exceptions\AccountAlreadyExistsException;
+use App\Exceptions\AccountNotFoundException;
+use App\Exceptions\InvalidAmountException;
+
+interface AccountService
+{
+    /**
+     * Create a new account with zero balance.
+     *
+     * @param int $accountId
+     * @throws AccountAlreadyExistsException
+     */
+    public function createAccount(int $accountId): void;
+
+    /**
+     * Deposit an amount into an account.
+     * If account does not exist, create it first.
+     *
+     * @param int $accountId
+     * @param float $amount
+     * @return float New balance after deposit
+     * @throws AccountNotFoundException
+     * @throws InvalidAmountException
+     */
+    public function deposit(int $accountId, float $amount): float;
+}
